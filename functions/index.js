a1871bf5bfd2b8c62262f426e8692909c10b40e9
Diana Nemyrovska <diana.nemyrovska@gmail.com>
2019-04-24 16:30:26 +0300

    Create and verify one time password functions

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const createUser = require("./create_user");
+const verifyPassword = require("./verify_password");
 const serviceAccount = require("./service_account.json");
+const requestPassword = require("./request_password");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -9,3 +11,5 @@ admin.initializeApp({
 });
 
 exports.createUser = functions.https.onRequest(createUser);
+exports.requestPassword = functions.https.onRequest(requestPassword);
+exports.verifyPassword = functions.https.onRequest(verifyPassword);
